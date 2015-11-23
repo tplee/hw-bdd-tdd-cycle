a310b1209d9534fbffc2b858ea3d90df10150358
Tommy <tplee@outlook.com>
2015-11-23 16:04:35 +0000

    assignment complete; added features from HW-BDD-CUCUMBER

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -61,4 +61,14 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+
+  def director
+    @movie = Movie.find(params[:id])
+    @director = @movie.director
+    if @director.blank?
+      flash[:warning] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path and return
+    end
+    @movies = @movie.similar_movies
+  end
 end
